1a9be88c1fa42d50e280f5053a62441a2dddccc2
Tiago Neves <tiagorockman@gmail.com>
2023-04-22 15:48:31 -0300

    Palindrome text

--- C#/Testes.cs
+++ C#/Testes.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace ConsoleAPP
@@ -8,10 +9,39 @@ namespace ConsoleAPP
     {
         public static void MainTestes()
         {
+            DateTime? start = null;
+            DateTime? end = null;
+            string texto = "ANA";
+            string invertido = new string(texto.Reverse().ToArray());
+            Console.WriteLine("Original Text = " + texto);
+
+            start = DateTime.Now;
+            Console.WriteLine("\n[libraryReverse]Reversed Text = " + invertido);
+            end = DateTime.Now;
+            TimeSpan ts = ((TimeSpan)(end - start));
+            Console.WriteLine(String.Format("The execution time is {0} ms", ts));
+
+            start = DateTime.Now;
+            Console.WriteLine("\n[func]Reversed Text = " + inverter(texto));
+            end = DateTime.Now;
+            Console.WriteLine(String.Format("The execution time is {0} ms", (end - start)));
+
+            if (texto == invertido)
+                Console.WriteLine("\nTexto é Palindromo");
+            else
+                Console.WriteLine("\nTexto Não é Palindromo");
 
         }
-        
+        internal static string inverter(string param)
+        {
+            string textInvert = "";
+            for (var i = param.Count() - 1; i >= 0; i--)
+            {
+                textInvert += param[i];
+            }
+            return textInvert;
+        }
 
-        
+
     }
 }
